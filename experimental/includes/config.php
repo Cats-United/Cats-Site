d74c45937c0ec79e32a7783141eb021c0783a883
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-21 17:48:30 +0800

    I'm sorry guys, but i got this from a tutorial lol

---
+++ experimental/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "localhost");     // host
+define("USER", "sec_user");    // db name
+define("PASSWORD", "eKcGZr59zAa2BEWU");    // db pass
+define("DATABASE", "CatsUnited_main");    // db name
+ 
+define("CAN_REGISTER", "any"); // for registering
+define("DEFAULT_ROLE", "mbm"); // becomes member for default
+ 
+define("SECURE", FALSE);    // set to true if site goes to HTTPS connection (MUM GET THE CAMERA!!!!!)
+?>
