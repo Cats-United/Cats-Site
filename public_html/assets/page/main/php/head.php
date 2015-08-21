6f26e263f422216d85c542176ad017db5f306049
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-21 18:03:36 +0800

    lines 3-7 work?

---
+++ public_html/assets/page/main/php/head.php
@@ -0,0 +1,29 @@
+<!-- meta & links-->
+<?php
+if ($_SERVER['PHP_SELF'] == "/index.php") {
+  echo'<title>CatsUnited</title>';
+}elseif ($_SERVER['PHP_SELF'] == "/projects/index.php") {
+  echo'<title>CatsUnited - Projects</title>';
+}
+?>
+<meta charset="utf-8">
+<meta name="description" content="CatsUnited Computers Home. Click here to start."/>
+<meta name="keywords" content="CatsUnited Software, CatsUnited, CatsUnited Computers"/>
+<meta name="author" content="CatsUnited Team"/>
+<link rel="shortcut icon" href="assets/cdn/main/img/ras/icon.png" type="image/png"/>
+<!-- css -->
+<link rel="stylesheet" href="assets/pages/main/css/main.css" type="text/css"/>
+<link rel="stylesheet" href="assets/pages/main/css/animation.css" type="text/css"/>
+<link rel="stylesheet" href="assets/pages/main/css/mobilecheck.css" type="text/css"/>
+<link rel="stylesheet" href="/assets/pages/main/css/hover-min.css" type="text/css"/>
+<link rel="stylesheet" href="assets/pages/main/css/projects.css"/>
+<link rel="stylesheet" href="assets/cdn/main/embed/Hover-master/hover-min.css" type="text/css"/>
+<link rel="stylesheet" href="assets/cdn/main/embed/vidplayer/videoplayer.css" type="text/css"/>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" type="text/css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" type="text/css"/>
+<!-- javascript -->
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script src="assets/pages/main/js/main.js"></script>
+<!-- cookies -->
+<!-- /head.php -->
