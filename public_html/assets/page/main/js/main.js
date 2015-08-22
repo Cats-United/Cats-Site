b646580569d7113a61c2f884ba82719e0a81e38d
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-22 18:02:59 +0800

    Update main.js

--- public_html/assets/page/main/js/main.js
+++ public_html/assets/page/main/js/main.js
@@ -1,1 +1,21 @@
-/* add things here */
+/* ===========================MAIN.JS=============================*/
+/* add a swaggy description here*/
+/* carousel */
+function carouselHide () {
+  document.getElementById("closeDialog").style.display = "none";
+}
+/* dialogs */
+if (location.pathname.substring(1) == index.php) {
+  $(function() {
+    $( "#under-construction" ).dialog({
+      resizable: false,
+      height:200,
+      modal: true,
+      buttons: {
+        "Close": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  });
+}
