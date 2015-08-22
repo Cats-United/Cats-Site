af39372d4f81f0d4d6fa17b061148adac64e8a89
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-22 18:01:52 +0800

    Create footer.php

---
+++ public_html/assets/page/main/php/footer.php
@@ -0,0 +1,34 @@
+<footer>
+			<!-- will go to assets/page/php/footer.php -->
+			<!-- Put language option. Planning to add English (duh), French, German, Dutch, Spanish, Portugese, Chinese, Japanese and Swedish, but more can be added (what about C++?) Will go to assets/page/php/langop/lang.php-->
+			<ul>
+				<li>
+					<h4>About</h4>
+				</li>
+				<li>
+					<a>About Us</a>
+				</li><li>
+					<a>Credits</a>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<h4>Other Places</h4>
+				</li>
+				<li>
+					<a><i class="fa fa-github"></i> Github</a>
+				</li>
+			</ul>
+			<select>
+				<!-- List of langauges -->
+				<option value="en" selected>English</option>
+				<option value="es">Espa&ntilde;ol</option>
+				<option value="ja">日本語</option>
+				<option value="ru">русский</option>
+			</select>
+			<br/>
+			<div id="license">
+			<img src="https://licensebuttons.net/l/by-nc/3.0/88x31.png"/>
+			<a>All these projects hosted on CatsUnited are under the <a href="https://creativecommons.org/licenses/by-nc/4.0/">Creative Commons BY-NC license.</a>
+			</div>
+		</footer>
