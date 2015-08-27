7dc8e20f4e9cede56fe3efcfed071e8ecfa988e3
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-27 16:28:35 +0800

    Create project-thumb.php

---
+++ public_html/assets/page/main/php/project-thumb.php
@@ -0,0 +1,10 @@
+<div class="thumb-container">
+  <div class="thumb-80">
+      <img src="<?php ?>" class="thumb-80"></img>
+  </div>
+  <div class="content">
+    <h4><?php ?></h4>
+    <div id="stats" class="thumb">
+    </div>
+  </div>
+</div>
