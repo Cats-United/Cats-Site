521d1f3aa2361dc0d79228c9bcf4c58ee2b848f1
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-28 18:09:32 +0800

    Update init.php

--- experimental/controlpanel/init.php
+++ experimental/controlpanel/init.php
@@ -9,12 +9,19 @@ CREATE TABLE `CatsUnited_main`.`members` (
 CREATE TABLE `CatsUnited_main`.`log_attempts` (
     `user_id` INT(11) NOT NULL,
     `time` VARCHAR(30) NOT NULL
-)   ENGINE=InnoDB
+)   ENGINE=InnoDB;
 CREATE TABLE `CatsUnited_main`.`projects` (
-  ENGINE = InnoDB;
-)
-/* other things */
-INSERT INTO `CatsUnited_main`.`members` VALUES(1, 'test_user', 'test@example.com',
-'00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
-'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `project_name` CHAR(128) NOT NULL,
+  `content` CHAR(131072)
+  `extension_name` CHAR(128) NOT NULL
+) ENGINE = InnoDB;
+CREATE TABLE `CatsUnited_main`.`news` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` CHAR(128) NOT NULL,
+  `content` CHAR(131072) NOT NULL
+) ENGINE = InnoDB;
+INSERT INTO `CatsUnited_main`.`members` VALUES(1, 'CatsUnited', 'CENSORED',
+'CENSORED','CENSORED');
+echo'meow';
 ?>
