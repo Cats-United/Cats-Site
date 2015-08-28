8837ff236d42c50d40536f7ffa2a7a339f7b7c26
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-28 18:11:16 +0800

    Update config.php

--- experimental/includes/config.php
+++ experimental/includes/config.php
@@ -1,11 +1,9 @@
 <?php
-define("HOST", "localhost");     // host
-define("USER", "sec_user");    // db name
-define("PASSWORD", "eKcGZr59zAa2BEWU");    // db pass
-define("DATABASE", "CatsUnited_main");    // db name
- 
-define("CAN_REGISTER", "any"); // for registering
-define("DEFAULT_ROLE", "mbm"); // becomes member for default
- 
-define("SECURE", FALSE);    // set to true if site goes to HTTPS connection (MUM GET THE CAMERA!!!!!)
+define("HOST", "localhost"); // sets the location of the host
+define("USER", "CatsUnited"); // create the first user
+define("PASSWORD", CENSORED);
+define("DATABASE", "CatsUnited_main"); // my database
+define("CAN_REGISTER", "select-cats"); // only Cats can add new users
+define("DEFAULT_ROLE", "members"); // Members have limited abilties. They can edit project pages. They will also become devs on the forum.
+define("SECURE", FALSE); // because of http
 ?>
