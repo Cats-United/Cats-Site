4be47c25dc67a6e8e8c066b62421d7db96c0cdc3
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-29 11:10:27 +0800

    Basic page

---
+++ experimental/includes/log_success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf8">
+    <title>success</title>
+  </head>
+  <body>
+    <h1>Redirecting...</h1>
+    <script>
+    window.location.replace(./ctrlpln/controlpanel.php);
+    </script>
+  </body>
+</html>
