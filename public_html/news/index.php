fac7755f20d995f2a5d27b8891753bbc0279fe8a
CatsUnited <Cats-United@users.noreply.github.com>
2015-08-29 11:12:01 +0800

    Basic page

---
+++ public_html/news/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+  </head>
+  <body class="news">
+    <div id="news">
+        <div id="news-title">
+          <h3>News Feed</h3>
+        </div>
+      <div id="content" class="news">
+          <table class="news">
+            <ul>
+              <?php
+              ?>
+            </ul>
+          </table>
+      </div>
+    </div>
+  </body>
+</html>
